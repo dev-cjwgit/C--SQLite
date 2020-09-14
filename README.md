7ac8905488c688fe8ff65edbb548181ed9f2f4de
DESKTOP-5DDDEKH\user <hyeonslove@kut.ac.kr>
2020-09-14 16:56:54 +0900

    C#_SQLite OpenSource

---
+++ README.md
@@ -0,0 +1,17 @@
+VisualStudio2019
+솔루션에서 "추가" - "기존 프로젝트" 후 "SQLiteComponent"의 SQLiteComponent.csproj 추가.
+사용하려는 Main 솔루션의 "참조" 우클릭 "참초추가" 후 "SQLiteComponent" 클릭후 확인
+SQLite sql = new SQLite(Environment.CurrentDirectory,"default"); // folder path와 생성할 파일이름지정.
+// path 맨 끝에 \가 없도록 주의
+
+sql.OpenDataBase() // SQL파일을 염(DB Server Connect과 비슷)
+sql.CreateDataBase() // SQL파일을 생성함(DB생성과 비슷)
+sql.ExecuteSQL() // SQL 명령문을 실행
+List<Dictionary<string, object>> list = sql.GetData(); // 명령문으로 실행한 SQL 데이터를 받아옴
+
+
+응용으로
+while (!sql.OpenDataBase())
+            {
+                while(!sql.CreateDataBase());
+등 순환을 이용하여 처리 가능.
---
+++ SQLiteProject1/Main.cs
@@ -0,0 +1,60 @@
+﻿using SQLiteComponent;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQLiteProject1
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            SQLite sql = new SQLite(Environment.CurrentDirectory,"default");
+            while (!sql.OpenDataBase())
+            {
+                while(!sql.CreateDataBase());
+                if (sql.OpenDataBase())
+                {
+                    /****************************************************************************/
+                    if (sql.ExecuteSQL("CREATE TABLE 'MEMBERS' ('age' int,'name' VARCHAR(50));"))
+                    {
+                        Console.WriteLine("SQL Execute  Success");
+                    }
+                    else
+                    {
+                        Console.WriteLine("SQL Execute Failed");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("SQL Open Failed");
+                }
+            }
+            sql.ExecuteSQL("INSERT INTO MEMBERS VALUES(25, \"최진우\"), (25, \"한국기술교육대학교\")");
+            sql.ExecuteSQL("SELECT * FROM MEMBERS;");
+            /* list[index][field]  */
+            /* list[int][string]  */
+            List<Dictionary<string, object>> list = sql.GetData();
+            foreach(var item in list)
+            {
+                foreach(var key in item.Keys)
+                {
+                    Console.Write(key + " " + item[key] + ", ");
+                }
+                Console.WriteLine();
+            }
+            
+        }
+    }
+}
